ef4eba9c70b30bb82433f7a9b868d760b1d12c84
gurmanbenipal <gurmxnbenipal@gmail.com>
2023-06-27 21:54:12 -0700

    done

---
+++ homework/ExpressIntroLab/main.js
@@ -0,0 +1,21 @@
+const students = [
+    {student: 'Jay', passed: true},
+    {student: 'Zaid', passed: false},
+    {student: 'Brooke', passed: false},
+    {student: 'Allen', passed: false}
+];
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/students', function(req, res) {
+    res.render('index', { students: students });
+});
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
